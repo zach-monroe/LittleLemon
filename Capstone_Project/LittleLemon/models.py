4b8a55dd7c1869223e419c2177e806071bf29190
zach-monroe <zmonroe1995@gmail.com>
2023-08-14 18:13:00 -0400

    first models added

--- Capstone_Project/LittleLemon/models.py
+++ Capstone_Project/LittleLemon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class BookingTable(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate= models.DateTimeField()
+
+class MenuTable(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
