3b23a78a176c4e6d6a0e9fe25d31558756ef2268
zach-monroe <zmonroe1995@gmail.com>
2023-08-21 16:24:50 -0400

    why didn't this work

--- Capstone_Project/Capstone_Project/urls.py
+++ Capstone_Project/Capstone_Project/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from LittleLemon.router import router
-import djoser.urls
-import djoser.urls.authtoken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
